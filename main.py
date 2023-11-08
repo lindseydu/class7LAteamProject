25ac3ccd2cc066968495a0ed71ff22408664184b
lindsey <jd5290@nyu.edu>
2023-11-08 14:14:52 -0500

    made a bar chart for the academic plan and pell grant

--- main.py
+++ main.py
@@ -18,3 +18,19 @@ st.write("Student Info")
 st.dataframe(StudentInfo)
 st.write("Student Term Info")
 st.dataframe(StudentTermInfo)
+
+joinedDataset = StudentInfo.merge(StudentCareerInfo,on=["Fake ID"], how="left")
+joinedDataset = joinedDataset.dropna(subset=["Received Pell grant?"])
+joinedDataset = pd.DataFrame(joinedDataset)
+
+joinedDataset = joinedDataset.drop(columns=["Birth date", "Job Full Time or Part Time", "Average tuition and fees", "Average disbursed", "Converted high school GPA", "Converted previous undergraduate GPA", "Years since last formal education", "Academic plan code", "Degree", "Career number", "Admit term code", "Admit term", "Total transferred units", "Start effective term", "Completion term code", "End effective term code", "Semesters", "Degree awarded"])
+
+# Assuming 'Received Pell grant?' contains 'Yes' or 'No' as string values
+joinedDataset['Received Pell grant?'] = joinedDataset['Received Pell grant?'].map({'Yes': True, 'No': False})
+
+# Now, you can safely check for True values and group by 'Academic plan'
+pell_grant_counts = joinedDataset[joinedDataset['Received Pell grant?']].groupby('Academic plan').size().reset_index(name='Count of Pell Grants')
+
+# Plot
+fig = px.bar(pell_grant_counts, x='Academic plan', y='Count of Pell Grants')
+st.plotly_chart(fig)
